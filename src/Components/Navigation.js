161ee2f15f049738d4161315b9b6d1c147eb1816
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-01-18 12:05:32 +0430

    Apply routing between deferent Views!

---
+++ src/Components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+const Navigation = () => (
+  <nav className="nav-bar">
+    <h1 className="math-title">Math-Magician</h1>
+    <div className="tags">
+      <Link to="/" className="link">Home</Link>
+      <Link to="/calculator" className="link">Calculator</Link>
+      <Link to="/quote" className="link">Quote</Link>
+    </div>
+  </nav>
+);
+
+export default Navigation;
---
+++ src/Components/Quote.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../App.css';
+
+const Quote = () => (
+  <div className="qouteInformation">
+    <h2>This is the Quote page of our App!</h2>
+    <p className="qoute">
+      Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit,
+      sed do eiusmod tempor incid
+      ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliqu
+      ex ea commodo consequat. Duis aute irure dolor in repreh
+      eit dolore eu fugiat nulla pariatur. Excepteur sint occ
+      cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit,
+      sed do eiusmod tempor incid
+      ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliqu
+      ex ea commodo consequat. Duis aute irure dolor in repreh
+      eit dolore eu fugiat nulla pariatur. Excepteur sint occ
+      cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit,
+      sed do eiusmod tempor incid
+    </p>
+  </div>
+);
+
+export default Quote;
